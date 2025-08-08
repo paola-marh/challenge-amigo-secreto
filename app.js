6552145dc789629709687375ced8dd189f700fb5
Paola <p.martinez.huircaman@gmail.com>
2025-08-08 11:41:18 -0400

    actualizacion js numero aleatorio

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+//
 function agregarAmigo() {
-    let nombre = document.getElementById()
+    let nombre = document.getElementById("amigo").value; 
+    if(nombre == "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombre);
+        console.log(nombre);
+        limpiar();    
+    }
 }
+// Limpiar campo de entrada 
+function limpiar() {
+    document.getElementById("amigo").value = ("");
+}
+//
+function sortearAmigo() {
+    let NumeroDeAmigos = amigos.length;
+     console.log(NumeroDeAmigos);
+    let aleatorio = Math.floor(Math.random()*NumeroDeAmigos);
+    console.log(aleatorio);
+    console.log(amigos [aleatorio]);
+}
+
